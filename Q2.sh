9e08b538dc06e797a8b9b88ce51700571cedd7aa
Aditi-Gp <136259100+Aditi-Gp@users.noreply.github.com>
2023-06-11 22:48:11 +0530

    Q2.sh

--- Q2.sh
+++ Q2.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+echo $(pwd)
+location= $(pwd)
+location=$1
+
+modified="$current_directory/Modified"
+mkdir -p "$modified"
+
+find "$location" -type f -name "*.txt" -exec bash -c '
+    for file do
+        base_name="${file%.*}"
+        cp "$file" "$modified/${base_name}.bak"
+
+        echo "$(cat "$file") $(date)" > "$modified/${base_name}.bak"
+    done
+' bash {} +
